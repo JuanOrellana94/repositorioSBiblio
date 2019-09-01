a69e6f3e5f6420e8a62b8a5d4e46e6c33cf3bea3
YaRo-93 <obdc321@hotmail.com>
2019-08-31 18:35:08 -0600

    Cambio visual en ver archivos
    Cambio visual en ver archivos

--- verArchivos.php
+++ verArchivos.php
@@ -1,8 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.flip-card {
+  background-color: transparent;
+  width: 300px;
+  height: 300px;
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  background-color: white;
+  color: black;
+}
+
+.flip-card-back {
+  background-color: #2980b9;
+  color: white;
+  transform: rotateY(180deg);
+}
+</style>
+</head>
+
+
+
+
+</html>
+
+
 <?php 
   session_start();
       include("src/libs/vars.php");
       include("src/libs/sessionControl/conection.php");
- $limite = 9;
+ $limite = 12;
   if (isset($_GET["pagina"])) { 
     $pagina  = $_GET["pagina"]; 
   } else {
@@ -98,7 +154,7 @@
       });
 </script>
 
-     <table class="table table-hover table-responsive">
+     <table class="table table-hover table-responsive" align="left">
     <?php      
             $selTable=mysqli_query($conexion,"SELECT * from tbl_documentos where titulo like '%$textBusqueda%' OR tagdocumento LIKE '%$textBusqueda%'
                 ORDER BY id_documento
@@ -113,35 +169,38 @@
            <?php if ($orden==1){ ?>               
         
             <tr>
-                   <td>
-              <div style="width:500px; padding:0px;" align="right"><?php echo "<img src='$dataLibros[doc_portada]' width='150' height='200'>" ?>
-                  
+                  <td style="width:350px;">
+              
+                <div align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Titulo:&nbsp;&nbsp;</strong><?php echo $dataLibros['titulo']; ?></div> <br>      
                <div style="width:245px;  float:right;" align="left"> 
-                 <strong>Titulo:</strong><?php echo $dataLibros['titulo']; ?> <br>             
-                <strong>Peso:</strong><?php echo $dataLibros['tamanio']; ?>  <br>     
-                <strong>Criterios:</strong><?php echo $dataLibros['tagdocumento']; ?>  <br>       
-            
-                <strong>Opciones:</strong><br><div class="btn-group" role="group" aria-label="Opciones">
+               <div  align="left">
+                 <div class="flip-card">
+                 <div class="flip-card-inner">
+                 <div class="flip-card-front">
+                <?php echo "<img src='$dataLibros[doc_portada]' width='250' height='300'>" ?> 
+                 </div>
+                  <div class="flip-card-back">
+                     <h1>Descripcion</h1> 
+                     <p><?php echo $dataLibros['descripcion'];?> </p>
+
+            <div align="center">
+                <div class="btn-group" role="group" aria-label="Opciones">
                                 <button type="button" class="btn btn-light" data-toggle="modal"
                                  data-target="#modalVerPdf" 
                                  data-varformdocumentoid="<?php echo $dataLibros['id_documento'];?>"                                 
                                  title="Mostrar Documento">
-                                    <img  src="img/icons/instruccion.png" width="20" height="20"><p>Leer</p>
+                                    <img  src="img/icons/instruccion.png" width="20" height="20"><p>Leer documento</p>
                                 </button> 
 
                                 </div>
+                               
+                   </div>
+                   </div>
+                  </div>  
+                 </div>
+              </div>
 
-                                 <div class="btn-group" role="group" aria-label="Opciones">
-                                <button type="button" class="btn btn-light" data-toggle="modal"
-                                 data-target="#modalVerDetalles" 
-                                 data-varformdocumentodes="<?php echo $dataLibros['descripcion'];?>"                                 
-                                 title="Mostrar Documento">
-                                    <img  src="img/icons/pregunta.png" width="20" height="20"><p>Ver detalles</p>
-                                </button>
-                            
-                                 
-                                </div>
-
+          
                                 <br>
 
                           <?php if (isset($_SESSION[ "Logeado" ])): ?>
@@ -175,42 +234,42 @@
 
 
               </div>
-              </div> 
+             
              </td>           
          
           <?php $orden=2;   }elseif ($orden==2) { # code...
-           ?>
-                      <td>
-              <div style="width:500px; padding:0px;" align="right"><?php echo "<img src='$dataLibros[doc_portada]' width='150' height='200'>" ?>
-                  
-               <div style="width:245px;  float:right;" align="left"> 
-                 <strong>Titulo:</strong><?php echo $dataLibros['titulo']; ?> <br>             
-                <strong>Peso:</strong><?php echo $dataLibros['tamanio']; ?>  <br>     
-                <strong>Criterios:</strong><?php echo $dataLibros['tagdocumento']; ?>  <br>        
-               
+           ?> <td style="width:350px;">
               
-                <strong>Opciones:</strong><br><div class="btn-group" role="group" aria-label="Opciones">
+                <div align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Titulo:&nbsp;&nbsp;</strong><?php echo $dataLibros['titulo']; ?></div> <br>      
+               <div style="width:245px;  float:right;" align="left"> 
+                 
+                 <div class="flip-card">
+                 <div class="flip-card-inner">
+                 <div class="flip-card-front">
+                <?php echo "<img src='$dataLibros[doc_portada]' width='250' height='300'>" ?> 
+                 </div>
+                  <div class="flip-card-back">
+                     <h1>Descripcion</h1> 
+                     <p><?php echo $dataLibros['descripcion'];?> </p>
+
+            <div align="center">
+                <div class="btn-group" role="group" aria-label="Opciones">
                                 <button type="button" class="btn btn-light" data-toggle="modal"
                                  data-target="#modalVerPdf" 
                                  data-varformdocumentoid="<?php echo $dataLibros['id_documento'];?>"                                 
                                  title="Mostrar Documento">
-                                    <img  src="img/icons/instruccion.png" width="20" height="20"><p>Leer</p>
+                                    <img  src="img/icons/instruccion.png" width="20" height="20"><p>Leer documento</p>
                                 </button> 
 
                                 </div>
+                               
+                   </div>
+                   </div>
+                 </div>
+              </div>
 
-                                 <div class="btn-group" role="group" aria-label="Opciones">
-                                <button type="button" class="btn btn-light" data-toggle="modal"
-                                 data-target="#modalVerDetalles" 
-                                 data-varformdocumentodes="<?php echo $dataLibros['descripcion'];?>"                                 
-                                 title="Mostrar Documento">
-                                    <img  src="img/icons/pregunta.png" width="20" height="20"><p>Ver detalles</p>
-                                </button>
-                            
-                                 
-                                </div>
-
-
+          
+                                <br>
                           <?php if (isset($_SESSION[ "Logeado" ])): ?>
                             
                           
@@ -237,40 +296,101 @@
                                 </div>
 
               </div> 
-             </td> 
-           <?php $orden=3;   }elseif ($orden==3) {
-           ?>
-                  <td>
-              <div style="width:500px; padding:0px;" align="right"><?php echo "<img src='$dataLibros[doc_portada]' width='150' height='200'>" ?>
-                  
+             </td>
+             <?php $orden=3;   }elseif ($orden==3) { # code...
+           ?> <td style="width:350px;">
+              
+                <div align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Titulo:&nbsp;&nbsp;</strong><?php echo $dataLibros['titulo']; ?></div> <br>      
                <div style="width:245px;  float:right;" align="left"> 
-                 <strong>Titulo:</strong><?php echo $dataLibros['titulo']; ?> <br>             
-                <strong>Peso:</strong><?php echo $dataLibros['tamanio']; ?>  <br>     
-                <strong>Criterios:</strong><?php echo $dataLibros['tagdocumento']; ?>  <br>        
-               
-                
-                <strong>Opciones:</strong><br><div class="btn-group" role="group" aria-label="Opciones">
+                 
+                 <div class="flip-card">
+                 <div class="flip-card-inner">
+                 <div class="flip-card-front">
+                <?php echo "<img src='$dataLibros[doc_portada]' width='250' height='300'>" ?> 
+                 </div>
+                  <div class="flip-card-back">
+                     <h1>Descripcion</h1> 
+                     <p><?php echo $dataLibros['descripcion'];?> </p>
+
+            <div align="center">
+                <div class="btn-group" role="group" aria-label="Opciones">
                                 <button type="button" class="btn btn-light" data-toggle="modal"
                                  data-target="#modalVerPdf" 
                                  data-varformdocumentoid="<?php echo $dataLibros['id_documento'];?>"                                 
                                  title="Mostrar Documento">
-                                    <img  src="img/icons/instruccion.png" width="20" height="20"><p>Leer</p>
+                                    <img  src="img/icons/instruccion.png" width="20" height="20"><p>Leer documento</p>
                                 </button> 
 
                                 </div>
+                               
+                   </div>
+                   </div>
+                 </div>
+              </div>
 
-                                 <div class="btn-group" role="group" aria-label="Opciones">
-                                <button type="button" class="btn btn-light" data-toggle="modal"
-                                 data-target="#modalVerDetalles" 
-                                 data-varformdocumentodes="<?php echo $dataLibros['descripcion'];?>"                                 
-                                 title="Mostrar Documento">
-                                    <img  src="img/icons/pregunta.png" width="20" height="20"><p>Ver detalles</p>
-                                </button>
+          
+                                <br>
+                          <?php if (isset($_SESSION[ "Logeado" ])): ?>
                             
-                                 
+                          
+
+                         <div style='color: green; font-weight: bold; text-align: center;'>OPCION DE ADMINISTRADOR</div><br>
+                          <div class="btn-group" role="group" aria-label="Opciones">
+                                <button type="button" class="btn btn-light" data-toggle="modal"
+                                 data-target="#modalEliminarpdf" 
+                                 data-varformdocumentoid="<?php echo $dataLibros['id_documento'];?>"
+                                 data-varformdocumentonom="<?php echo $dataLibros['nombre_archivo'];?>"
+                                 data-varformdocumentotit="<?php echo $dataLibros['titulo'];?>"  
+
+                                 title="Eliminar PDF">
+                                    <img  src="img/icons/itemD.png" width="35" height="30">
+                                </button>
+                               <button type="button" class="btn btn-light"  data-toggle="modal" data-target="#fotografiaModal"
+                            data-varformdocumentoid="<?php echo $dataLibros['id_documento'];?>"
+                              data-varformdocumentotit="<?php echo $dataLibros['titulo'];?>"
+                             data-varformdocumentopor="<?php echo $dataLibros['doc_portada'];?>"
+                                title="Portada del Documento"   
+                           ><img src="img/icons/BookCover.png" width="35" height="30"></button>
+                            
+                        <?php endif ?>         
                                 </div>
 
+              </div> 
+             </td>  
+           <?php $orden=4;   }elseif ($orden==4) {
+           ?>
+                <td style="width:350px;">
+              
+                <div align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Titulo:&nbsp;&nbsp;</strong><?php echo $dataLibros['titulo']; ?></div> <br>      
+               <div style="width:245px;  float:right;" align="left"> 
+                 
+                 <div class="flip-card">
+                 <div class="flip-card-inner">
+                 <div class="flip-card-front">
+                <?php echo "<img src='$dataLibros[doc_portada]' width='250' height='300'>" ?> 
+                 </div>
+                  <div class="flip-card-back">
+                     <h1>Descripcion del Documento</h1> 
+                     <p><?php echo $dataLibros['descripcion'];?> </p>
+                   
+            <div align="center">
+                <div class="btn-group" role="group" aria-label="Opciones">
+                                <button type="button" class="btn btn-light" data-toggle="modal"
+                                 data-target="#modalVerPdf" 
+                                 data-varformdocumentoid="<?php echo $dataLibros['id_documento'];?>"                                 
+                                 title="Mostrar Documento">
+                                    <img  src="img/icons/instruccion.png" width="20" height="20"><p>Leer documento</p>
+                                </button> 
 
+                                </div>
+                               
+                   </div>
+                   </div>
+                 </div>
+              </div>
+
+          
+                                <br>
                           <?php if (isset($_SESSION[ "Logeado" ])): ?>
                             
                           
